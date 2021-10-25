c9d5855257452787a5df3cb1ad7547cfe380f25d
Olu "Jide" Otuyelu <jotuyelu@yahoo.com>
2021-10-25 09:33:59 -0400

    Update birthdayBorder.kt
    Accept border string and repetition number

--- birthdayBorder.kt
+++ birthdayBorder.kt
@@ -1,11 +1,14 @@
 fun main() {
-    printBorder()
-    println("Happy Birthday, Ayomide!")
-    printBorder()
+    val border = "`-._,-'"
+    val timesToRepeat = 4
+    printBorder(border, timesToRepeat)
+    println("  Happy Birthday, Jhansi!")
+    printBorder(border, timesToRepeat)
 }
 
-fun printBorder() {
-    repeat(23) {
-        print("=")
+fun printBorder(border: String, timesToRepeat: Int) {
+    repeat(timesToRepeat) {
+        print(border)
     }
+    println()
 }
