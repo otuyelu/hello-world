0382e1d8f21d9e79344d970a95f9530acf9e591b
Olu "Jide" Otuyelu <jotuyelu@yahoo.com>
2021-10-25 07:49:46 -0400

    Create Kotlin.kt
    Start of Kotlin tutorial

---
+++ Kotlin.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val age = 5
+    println("Happy Birthday, Rover!")
+    
+    // Let's print a cake!
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    
+    // This prints an empty line.
+    println("")
+
+    println("You are already ${age}!")
+    println("${age} is the very best age to celebrate!")
+}
