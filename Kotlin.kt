58b84254433459123d1efd3ee5f3a3506222978f
Olu "Jide" Otuyelu <jotuyelu@yahoo.com>
2021-10-25 07:58:12 -0400

    Update Kotlin.kt
    print age in days

--- Kotlin.kt
+++ Kotlin.kt
@@ -1,5 +1,8 @@
 fun main() {
+    // variable declared with val are treated as constants
+    // their values can not be changed later
     val age = 5
+    var ageInDays = age * 365
     println("Happy Birthday, Rover!")
     
     // Let's print a cake!
@@ -13,6 +16,6 @@ fun main() {
     // This prints an empty line.
     println("")
 
-    println("You are already ${age}!")
+    println("You are already ${age} years that is ${ageInDays} days old!")
     println("${age} is the very best age to celebrate!")
 }
